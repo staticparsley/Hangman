c1a714dd1936c45f38ba907ca6980dad0a4b7e31
Ruben Marin <staticparsley92@gmail.com>
2018-10-02 19:01:51 -0700

    fixed callback issue with request

--- app.js
+++ app.js
@@ -12,15 +12,10 @@ window.addEventListener('keypress', (e) => {
     guessesEl.textContent = game1.statusMessage;
 });
 
-const request = new XMLHttpRequest();
-
-request.addEventListener('readystatechange',(e) => {
-    if(e.target.readyState === 4 && e.target.status === 200){
-        const data = JSON.parse(e.target.responseText);
-    } else if(e.target.readyState){
-        console.log("error");
+getPuzzle((error, puzzle) => {
+    if (error) {
+        console.log(`Error: ${error}`);
+    } else {
+        console.log(puzzle);
     }
 });
-
-request.open('GET','http://puzzle.mead.io/puzzle');
-request.send();
