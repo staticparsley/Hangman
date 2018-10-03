90e0ef8d6ee54e6537b6f1f0fb74963eb266320b
Ruben Marin <staticparsley92@gmail.com>
2018-10-03 03:09:44 -0700

    added promises to project

--- app.js
+++ app.js
@@ -12,10 +12,8 @@ window.addEventListener('keypress', (e) => {
     guessesEl.textContent = game1.statusMessage;
 });
 
-getPuzzle((error, puzzle) => {
-    if (error) {
-        console.log(`Error: ${error}`);
-    } else {
-        console.log(puzzle);
-    }
+getPuzzle('2').then((puzzle) => {
+    console.log(puzzle)
+}, (err) => {
+    console.log(`Error: ${err}`)
 });
